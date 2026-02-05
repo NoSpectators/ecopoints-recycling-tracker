f8078f05b362f47f099f7cacdcc9d7044e16bc11
NoSpectators <john.c.erickson@gmail.com>
2026-02-05 13:33:36 -0500

    household class

--- HouseHold.java
+++ HouseHold.java
@@ -8,5 +8,41 @@ import java.util.List;
  */
 
 public class HouseHold implements Serializable {
+    private String id;
+    private String name;
+    private String address;
+    private LocalDate joinDate;
+    private List<RecyclingEvent> events;
+    private double totalPoints;
+
+    public HouseHold(String id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.joinDate = LocalDate.now();
+        this.events = new ArrayList<>(); // initialized (empty, not null)
+        this.totalPoints = 0.0;
+    }
+
+    // getters
+    public String getId() { return this.id; }
+    public String getName() { return this.name; }
+    public String getAddress() { return this.address; }
+    public LocalDate getJoinDate() { return this.joinDate; }
+    public List<RecyclingEvent> getEvents() { return this.events; }
+    public double getTotalPoints() { return this.totalPoints; }
+
+    public void addEvent(RecyclingEvent event) {
+        this.events.add(event);
+        this.totalPoints += event.getEcoPoints();
+    }
+
+    public double getTotalWeight() {
+        double total = 0.0;
+        for (RecyclingEvent event : events) {
+            total += event.getWeight();
+        }
+        return total;
+    }
 
 }
---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#export CLASSPATH=$CLASSPATH:/home/john/my-projects/java-projects/ecopoints-recycling-tracker
+
+javac *.java
+
